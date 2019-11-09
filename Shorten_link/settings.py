b2e7f8b0db9222ce2f939e4eaab5efd38ee698c7
unknown <korrarkk@gmail.com>
2019-11-09 16:05:59 +0100

    make it better

--- Shorten_link/settings.py
+++ Shorten_link/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3f7(h09jj-=q599t!%_s_8z=o1eq9!2*o6a!7lr#-@g=#2)v$1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' ]
 
 
 # Application definition
@@ -119,4 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
